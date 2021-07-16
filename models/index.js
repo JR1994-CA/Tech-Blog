792d622a60c816c9816cccb4734df261c93713e3
Duality <jramirezarroyo21@gmail.com>
2021-07-16 02:40:53 -0700

    Rest of the Models

---
+++ models/index.js
@@ -0,0 +1,48 @@
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
+
+// create associations
+
+//User
+
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL',
+    hooks: true,
+});
+
+//Comment
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL',
+    hooks: true,
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL',
+    hooks:true,
+});
+  
+//Post
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL',
+  hooks: true,
+});
+
+module.exports = { User, Post, Comment };
