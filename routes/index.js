b0606de2943ffca55a3b196c9c16695d219cca15
Duality <jramirezarroyo21@gmail.com>
2021-07-16 03:57:17 -0700

    User, post, comment, and indexes routes

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+// API routes folder
+const apiRoutes = require('./api');
+// Homepage routes
+const homeRoutes = require('./home-routes');
+// Dashboard Routes
+const dashboardRoutes = require('./dashboard-routes');
+
+router.use('/', homeRoutes);
+
+router.use('/api', apiRoutes);
+
+router.use('/dashboard', dashboardRoutes);
+
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
